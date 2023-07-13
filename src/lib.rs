5122ced3641b0861ad7eaf5ae81b59f564ca3af4
pfhaupt <pfhaupt@gmx.net>
2023-07-13 19:40:17 +0200

    Add Matrix subtraction

--- src/lib.rs
+++ src/lib.rs
@@ -61,6 +61,20 @@ impl Matrix {
             Ok(m)
         }
     }
+
+    pub fn sub(&self, other: &Matrix) -> Result<Self, &str> {
+        if self.rows != other.rows || self.cols != other.cols {
+            Err("Mismatch in dimensions!")
+        } else {
+            let mut m = Matrix::new(self.rows, self.cols);
+            for i in 0..self.rows {
+                for j in 0..self.cols {
+                    m.set_unchecked(i, j, self.get_unchecked(i, j) - other.get_unchecked(i, j));
+                }
+            }
+            Ok(m)
+        }
+    }
 }
 
 #[cfg(test)]
@@ -106,6 +120,38 @@ mod tests {
                 }
             }
             let result = m1.add(&m2);
+            assert_eq!(result.unwrap().content, control_vector);
+        }
+    }
+
+    #[test]
+    fn sub_not_same_dim() {
+        let m1 = Matrix::new(5, 5);
+        let m2 = Matrix::new(6, 6);
+        let r1 = m1.sub(&m2);
+        let r2 = m2.sub(&m1);
+        assert!(r1.is_err());
+        assert!(r2.is_err());
+    }
+
+    #[test]
+    fn sub_same_dim() {
+        for _ in 0..100 {
+            let rows: usize = rand::thread_rng().gen_range(5..100);
+            let cols: usize = rand::thread_rng().gen_range(5..100);
+            let mut m1 = Matrix::new(rows, cols);
+            let mut m2 = Matrix::new(rows, cols);
+            let mut control_vector = vec![0.0; rows * cols];
+            for i in 0..m1.rows {
+                for j in 0..m1.cols {
+                    let v1 = rand::thread_rng().gen_range(0.0..10.0);
+                    let v2 = rand::thread_rng().gen_range(0.0..10.0);
+                    m1.set_unchecked(i, j, v1);
+                    m2.set_unchecked(i, j, v2);
+                    control_vector[m1.get_index(i, j)] = v1 - v2;
+                }
+            }
+            let result = m1.sub(&m2);
             assert_eq!(result.unwrap().content, control_vector);
         }
     }
