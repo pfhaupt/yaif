06ea530d3101ca3af837938553b49103b1ab8548
pfhaupt <pfhaupt@gmx.net>
2023-07-17 01:54:52 +0200

    Add OpenCL support
    So far I've written kernels for Matrix Addition,
    Matrix-Scalar-Multiplication, and 4 versionf of
    Matrix-Matrix-Multiplication.
    MMM only works for matrices with dimensions that are multiples of 32x32
    so far. I am currently looking for a way to fix that, and make it accept
    all matrices.

---
+++ src/kernel.rs
@@ -0,0 +1,331 @@
+#![allow(dead_code)]
+
+use opencl3::event::Event;
+use opencl3::command_queue::CommandQueue;
+use opencl3::kernel::{ExecuteKernel, Kernel};
+use opencl3::memory::Buffer;
+use opencl3::Result;
+
+
+const MMUL_VERSION: usize = 2;
+
+const TS: usize = 16;
+
+const WPT: usize = 4;
+const RTS: usize = TS / WPT;
+
+const TSM: usize = 8;
+const TSN: usize = TSM;
+const TSK: usize = 8;
+const WPTM: usize = 4;
+const WPTN: usize = WPTM;
+const RTSM: usize = TSM / WPTM;
+const RTSN: usize = TSN / WPTN;
+const LPTA: usize = (TSK * TSM) / (RTSM * RTSN);
+const LPTB: usize = (TSK * TSN) / (RTSM * RTSN);
+
+
+pub const ADD_NAME: &str = "matrix_add";
+pub const ADD_SOURCE: &str = r#"
+kernel void matrix_add(
+    global float* z,
+    global float const* x,
+    global float const* y)
+{
+    size_t i = get_global_id(0);
+    z[i] = x[i] + y[i];
+}"#;
+
+
+pub const MUL_SCALAR_NAME: &str = "matrix_mul_scalar";
+pub const MUL_SCALAR_SOURCE: &str = r#"
+kernel void matrix_mul_scalar(
+    global float* x,
+    float y)
+{
+    size_t i = get_global_id(0);
+    x[i] = x[i] * y;
+}"#;
+
+
+pub const MUL_MATRIX_NAME: &str = "matrix_mul_matrix";
+pub const MUL_MATRIX_SOURCE: &str =
+if MMUL_VERSION == 1 {
+r#"
+kernel void matrix_mul_matrix(
+    const int M,
+    const int N,
+    const int K,
+    global float const* A,
+    global float const* B,
+    global float* C)
+{
+    const int globalRow = get_global_id(0);
+    const int globalCol = get_global_id(1);
+
+    float acc = 0.0f;
+    for (int k = 0; k < K; k++) {
+        acc += A[k * M + globalRow] * B[globalCol * K + k];
+    }
+    C[globalCol * M + globalRow] = acc;
+}
+"#
+} else if MMUL_VERSION == 2 { // Version 2 (working)
+r#"
+#define TS 16
+kernel void matrix_mul_matrix(
+    const int M,
+    const int N,
+    const int K,
+    global float const* A,
+    global float const* B,
+    global float* C,
+    local float* Asub,
+    local float* Bsub)
+{
+    const int row = get_local_id(0); // Local row ID (max: TS)
+    const int col = get_local_id(1); // Local col ID (max: TS)
+    const int globalRow = TS*get_group_id(0) + row; // Row ID of C (0..M)
+    const int globalCol = TS*get_group_id(1) + col; // Col ID of C (0..N)
+
+    // Initialise the accumulation register
+    float acc = 0.0f;
+    
+    // Loop over all tiles
+    const int numTiles = K/TS;
+    for (int t=0; t<numTiles; t++) {
+
+        // Load one tile of A and B into local memory
+        const int tiledRow = TS*t + row;
+        const int tiledCol = TS*t + col;
+        Asub[col * TS + row] = A[tiledCol*M + globalRow];
+        Bsub[col * TS + row] = B[globalCol*K + tiledRow];
+
+        // Synchronise to make sure the tile is loaded
+        barrier(CLK_LOCAL_MEM_FENCE);
+
+        // Perform the computation for a single tile
+        for (int k=0; k<TS; k++) {
+            acc += Asub[k * TS + row] * Bsub[col * TS + k];
+        }
+
+        // Synchronise before loading the next tile
+        barrier(CLK_LOCAL_MEM_FENCE);
+    }
+
+    // Store the final result in C
+    C[globalCol*M + globalRow] = acc;
+}
+"#
+} else if MMUL_VERSION == 3 { // Version 3 (working)
+r#"
+#define TS 8
+#define WPT 4
+#define RTS (TS/WPT)
+kernel void matrix_mul_matrix(
+    const int M,
+    const int N,
+    const int K,
+    global float const* A,
+    global float const* B,
+    global float* C,
+    local float* Asub,
+    local float* Bsub)
+{
+    const int row = get_local_id(0); // Local row ID (max: TS)
+    const int col = get_local_id(1); // Local col ID (max: TS/WPT == RTS)
+    const int globalRow = TS*get_group_id(0) + row; // Row ID of C (0..M)
+    const int globalCol = TS*get_group_id(1) + col; // Col ID of C (0..N)
+
+    // Initialise the accumulation registers
+    float acc[WPT];
+    for (int w=0; w<WPT; w++) {
+        acc[w] = 0.0f;
+    }
+
+    // Loop over all tiles
+    const int numTiles = K/TS;
+    for (int t=0; t<numTiles; t++) {
+
+        // Load one tile of A and B into local memory
+        for (int w=0; w<WPT; w++) {
+            const int tiledRow = TS*t + row;
+            const int tiledCol = TS*t + col;
+            Asub[(col + w*RTS) * TS + row] = A[(tiledCol + w*RTS)*M + globalRow];
+            Bsub[(col + w*RTS) * TS + row] = B[(globalCol + w*RTS)*K + tiledRow];
+        }
+
+        // Synchronise to make sure the tile is loaded
+        barrier(CLK_LOCAL_MEM_FENCE);
+
+        // Perform the computation for a single tile
+        for (int k=0; k<TS; k++) {
+            for (int w=0; w<WPT; w++) {
+                acc[w] += Asub[k * TS + row] * Bsub[(col + w*RTS) * TS + k];
+            }
+        }
+
+        // Synchronise before loading the next tile
+        barrier(CLK_LOCAL_MEM_FENCE);
+    }
+
+    // Store the final results in C
+    for (int w=0; w<WPT; w++) {
+        C[(globalCol + w*RTS)*M + globalRow] = acc[w];
+    }
+}
+"#
+} else if MMUL_VERSION == 4 { // Version 4 (working, but slow)
+r#"
+#define MOD2(x,y) ((x) % (y))
+#define DIV2(x,y) ((x) / (y))
+#define TS 16
+#define WPT 4
+#define RTS (TS/WPT)
+#define TSM 8
+#define TSN TSM
+#define TSK 8
+#define WPTM 4
+#define WPTN WPTM
+#define RTSM (TSM/WPTM)
+#define RTSN (TSN/WPTN)
+#define LPTA ((TSK*TSM)/(RTSM*RTSN))
+#define LPTB ((TSK*TSN)/(RTSM*RTSN))
+kernel void matrix_mul_matrix(
+    const int M,
+    const int N,
+    const int K,
+    global float const* A,
+    global float const* B,
+    global float* C,
+    local float* Asub,
+    local float* Bsub)
+{
+    // Thread identifiers
+    const int tidm = get_local_id(0); // Local row ID (max: TSM/WPTM == RTSM)
+    const int tidn = get_local_id(1); // Local col ID (max: TSN/WPTN == RTSN)
+    const int offsetM = TSM*get_group_id(0); // Work-group offset
+    const int offsetN = TSN*get_group_id(1); // Work-group offset
+
+    // Allocate register space
+    float Areg;
+    float Breg[WPTN];
+    float acc[WPTM][WPTN];
+
+    // Initialise the accumulation registers
+    #pragma unroll
+    for (int wm=0; wm<WPTM; wm++) {
+        #pragma unroll
+        for (int wn=0; wn<WPTN; wn++) {
+            acc[wm][wn] = 0.0f;
+        }
+    }
+
+    // Loop over all tiles
+    const int numTiles = K/TSK;
+    int t=0;
+    do {
+
+        // Load one tile of A and B into local memory
+        #pragma unroll
+        for (int la=0; la<LPTA; la++) {
+            int tid = tidn*RTSM + tidm;
+            volatile int id = la*RTSN*RTSM + tid;
+            int row = MOD2(id,TSM);
+            int col = DIV2(id,TSM);
+            int tiledIndex = TSK*t + col;
+            Asub[col * TSK + row] = A[tiledIndex*M + offsetM + row];
+            Bsub[row * TSN + col] = B[tiledIndex*N + offsetN + row];
+        }
+
+        // Synchronise to make sure the tile is loaded
+        barrier(CLK_LOCAL_MEM_FENCE);
+
+        // Loop over the values of a single tile
+        for (int k=0; k<TSK; k++) {
+
+            // Cache the values of Bsub in registers
+            #pragma unroll
+            for (int wn=0; wn<WPTN; wn++) {
+                int col = tidn + wn*RTSN;
+                Breg[wn] = Bsub[col * TSN + k];
+            }
+
+            // Perform the computation
+            #pragma unroll
+            for (int wm=0; wm<WPTM; wm++) {
+                int row = tidm + wm*RTSM;
+                Areg = Asub[k * TSK + row];
+                #pragma unroll
+                for (int wn=0; wn<WPTN; wn++) {
+                    acc[wm][wn] += Areg * Breg[wn];
+                }
+            }
+        }
+
+        // Synchronise before loading the next tile
+        barrier(CLK_LOCAL_MEM_FENCE);
+
+        // Next tile
+        t++;
+    } while (t<numTiles);
+
+    // Store the final results in C
+    #pragma unroll
+    for (int wm=0; wm<WPTM; wm++) {
+        int globalRow = offsetM + tidm + wm*RTSM;
+        #pragma unroll
+        for (int wn=0; wn<WPTN; wn++) {
+            int globalCol = offsetN + tidn + wn*RTSN;
+            C[globalCol*M + globalRow] = acc[wm][wn];
+        }
+    }
+}
+"#
+} else {
+    ""
+};
+
+
+
+pub fn get_mmul_kernel_event(kernel: &Kernel, queue: &CommandQueue, m: u32, n: u32, k: u32, a: &Buffer<f32>, b: &Buffer<f32>, c: &Buffer<f32>) -> Result<Event> {
+    let mut exec_kernel = ExecuteKernel::new(&kernel);
+    let event = exec_kernel.set_arg(&m)
+        .set_arg(&n)
+        .set_arg(&k)
+        .set_arg(a)
+        .set_arg(b)
+        .set_arg(c);
+    let result = match MMUL_VERSION {
+        1 => {
+            event.set_global_work_sizes(&[m as usize, n as usize, 1])
+                .set_local_work_sizes(&[TS, TS, 1])
+                .enqueue_nd_range(&queue)?
+        },
+        2 => {
+            event.set_arg_local_buffer(TS * TS * 4)
+                .set_arg_local_buffer(TS * TS * 4)
+                .set_global_work_sizes(&[m as usize, n as usize, 1])
+                .set_local_work_sizes(&[TS, TS, 1])
+                .enqueue_nd_range(&queue)?
+        },
+        3 => {
+            event.set_arg_local_buffer(TS * TS * 4)
+                .set_arg_local_buffer(TS * TS * 4)
+                .set_global_work_sizes(&[m as usize, n as usize / WPT, 1])
+                .set_local_work_sizes(&[TS, TS / WPT, 1])
+                .enqueue_nd_range(&queue)?
+        },
+        4 => {
+            event.set_arg_local_buffer(TSK * TSM * 4)
+                .set_arg_local_buffer(TSN * (TSK + 2) * 4)
+                .set_global_work_sizes(&[m as usize / WPTM, n as usize/ WPTN, 1])
+                .set_local_work_sizes(&[RTSM, RTSN, 1])
+                .enqueue_nd_range(&queue)?
+        },
+        _ => {
+            panic!("Invalid MMUL_VERSION selected! {}", MMUL_VERSION);
+        }
+    };
+    Ok(result)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,83 @@
+#![allow(dead_code, unused, non_snake_case)]
+
+#[feature(mmul_v2)]
+
 use yaif::matrix::Matrix;
+use core::panic;
 use std::time::Instant;
 
 use opencl3::command_queue::{CommandQueue, CL_QUEUE_PROFILING_ENABLE};
 use opencl3::context::Context;
 use opencl3::device::{get_all_devices, Device, CL_DEVICE_TYPE_GPU};
 use opencl3::kernel::{ExecuteKernel, Kernel};
-use opencl3::memory::{Buffer, CL_MEM_READ_ONLY, CL_MEM_WRITE_ONLY};
+use opencl3::memory::{Buffer, CL_MEM_READ_ONLY, CL_MEM_WRITE_ONLY, CL_MEM_READ_WRITE};
 use opencl3::program::Program;
-use opencl3::types::{cl_event, cl_float, CL_BLOCKING, CL_NON_BLOCKING};
+use opencl3::types::{cl_event, cl_float, cl_uint, CL_BLOCKING, CL_NON_BLOCKING};
 use opencl3::Result;
 use std::ptr;
 
-fn init_mat(size: usize) -> (Matrix, Matrix) {
+use rand::Rng;
+
+fn init_single_mat(size: usize) -> Matrix {
     let mut m1 = Matrix::new(size, size);
-    let mut m2 = Matrix::new(size, size);
     m1.fill(1.0);
-    m2.fill(1.0);
-    (m1, m2)
+    m1
 }
 
-fn test_solo(tests: usize, size: usize) {
+fn init_mat(size: usize) -> (Matrix, Matrix) {
+    (init_single_mat(size), init_single_mat(size))
+}
+
+fn time_basic(tests: usize, size: usize, which: &str) {
     for _ in 0..tests {
-        let (m1, m2) = init_mat(size);
-        let r = m1.add(&m2).unwrap();
+        match which {
+            "add" => {
+                let (m1, m2) = init_mat(size);
+                let _r = m1.add(&m2).unwrap();
+            },
+            "smul" => {
+                let (mut m1, _) = init_mat(size);
+                let scalar = rand::thread_rng().gen_range(0.0..10.0);
+                m1.multiply_scalar(scalar);
+            },
+            "mmul" => {
+                let (m1, m2) = init_mat(size);
+                let _r = m1.multiply(&m2).unwrap();
+            },
+            _ => {
+                unimplemented!("{}", which);
+            }
+        }
     }
 }
 
-const PROGRAM_SOURCE: &str = r#"
-kernel void matrix_add(global float* z,
-    global float const* x,
-    global float const* y)
-{
-    const size_t local_dispatch_size = get_local_size(0); // same on y axis
-    const size_t global_idx_x = get_global_id(0);
-    const size_t global_idx_y = get_global_id(1);
-    
-    const i = global_idx_x + global_idx_y * local_dispatch_size;
-    z[i] = x[i] + y[i];
-}"#;
-
-const KERNEL_NAME: &str = "matrix_add";
-
-fn test_opencl(kernel: &Kernel, context: &Context, queue: &CommandQueue, tests: usize, size: usize) -> Result<u64> {
+fn test_add(kernel: &Kernel, context: &Context, queue: &CommandQueue, tests: usize, size: usize) -> Result<u64> {
     let array_size = size * size;
-    let mut x = Buffer::<cl_float>::create(&context, CL_MEM_READ_ONLY, array_size, ptr::null_mut())?;
-    let mut y = Buffer::<cl_float>::create(&context, CL_MEM_READ_ONLY, array_size, ptr::null_mut())?;
-    let z = Buffer::<cl_float>::create(&context, CL_MEM_WRITE_ONLY, array_size, ptr::null_mut())?;
+
+    let mut gs = (array_size / 64) * 64;
+    if gs == 0 || !array_size.is_power_of_two() { 
+        gs += 64;
+    }
+
+    let mut x = Buffer::<cl_float>::create(&context, CL_MEM_READ_ONLY, gs, ptr::null_mut())?;
+    let mut y = Buffer::<cl_float>::create(&context, CL_MEM_READ_ONLY, gs, ptr::null_mut())?;
+    let z = Buffer::<cl_float>::create(&context, CL_MEM_WRITE_ONLY, gs, ptr::null_mut())?;
+    
     let mut dur = 0;
+
     for _ in 0..tests {
         let (m1, m2) = init_mat(size);
         
         let _x_write_event = queue.enqueue_write_buffer(&mut x, CL_BLOCKING, 0, &m1.get_all(), &[])?;
-        let y_write_event = queue.enqueue_write_buffer(&mut y, CL_NON_BLOCKING, 0, &m2.get_all(), &[])?;
+        let y_write_event = queue.enqueue_write_buffer(&mut y, CL_BLOCKING, 0, &m2.get_all(), &[])?;
         
-        let gs = (array_size / 512 + 1) * 512;
         let kernel_event = 
             ExecuteKernel::new(&kernel)
                 .set_arg(&z)
                 .set_arg(&x)
                 .set_arg(&y)
                 .set_global_work_sizes(&[gs, 1, 1])
-                .set_local_work_sizes(&[512, 1, 1])
+                .set_local_work_sizes(&[64, 1, 1])
                 .set_wait_event(&y_write_event)
                 .enqueue_nd_range(&queue)?;
         
@@ -69,10 +86,101 @@ fn test_opencl(kernel: &Kernel, context: &Context, queue: &CommandQueue, tests: 
 
         let mut r1 = vec![0.0; array_size];
         
-        let read_event = queue.enqueue_read_buffer(&z, CL_NON_BLOCKING, 0, &mut r1, &events)?;
+        let read_event = queue.enqueue_read_buffer(&z, CL_BLOCKING, 0, &mut r1, &events)?;
         read_event.wait()?;
         let mut r = Matrix::new(size, size);
         r.fill_vec(&r1);
+
+        assert_eq!(r, m1.add(&m2).unwrap(), "OpenCL Matrix Matrix Addition is not working properly!");
+
+        let start_time = kernel_event.profiling_command_start()?;
+        let end_time = kernel_event.profiling_command_end()?;
+        let duration = end_time - start_time;
+        dur += duration;
+    }
+    Ok(dur)
+}
+
+fn test_smul(kernel: &Kernel, context: &Context, queue: &CommandQueue, tests: usize, size: usize) -> Result<u64> {
+    let array_size = size * size;
+
+    let mut gs = (array_size / 64) * 64;
+    if gs == 0 || !array_size.is_power_of_two() { 
+        gs += 64;
+    }
+
+    let mut x = Buffer::<cl_float>::create(&context, CL_MEM_READ_WRITE, gs, ptr::null_mut())?;
+
+    let mut dur = 0;
+
+    for _ in 0..tests {
+        let (mut m1, _) = init_mat(size);
+        let scalar: cl_float = rand::thread_rng().gen_range(0..10) as f32;
+        
+        let _x_write_event = queue.enqueue_write_buffer(&mut x, CL_BLOCKING, 0, &m1.get_all(), &[])?;
+        let kernel_event = 
+            ExecuteKernel::new(&kernel)
+                .set_arg(&x)
+                .set_arg(&scalar)
+                .set_global_work_sizes(&[gs, 1, 1])
+                .set_local_work_sizes(&[64, 1, 1])
+                .enqueue_nd_range(&queue)?;
+        
+        let mut events: Vec<cl_event> = Vec::default();
+        events.push(kernel_event.get());
+
+        let mut r1 = vec![0.0; array_size];
+        
+        let read_event = queue.enqueue_read_buffer(&x, CL_BLOCKING, 0, &mut r1, &events)?;
+        read_event.wait()?;
+        let mut r = Matrix::new(size, size);
+        r.fill_vec(&r1);
+
+        m1.multiply_scalar(scalar);
+        assert_eq!(r, m1, "OpenCL Matrix Scalar Multiplication is not working properly!");
+
+        let start_time = kernel_event.profiling_command_start()?;
+        let end_time = kernel_event.profiling_command_end()?;
+        let duration = end_time - start_time;
+        dur += duration;
+    }
+    Ok(dur)
+}
+
+fn test_mmul(kernel: &Kernel, context: &Context, queue: &CommandQueue, tests: usize, size: usize) -> Result<u64> {
+    let array_size = size * size;
+
+    let mut A = Buffer::<cl_float>::create(&context, CL_MEM_READ_ONLY, array_size, ptr::null_mut())?;
+    let mut B = Buffer::<cl_float>::create(&context, CL_MEM_READ_ONLY, array_size, ptr::null_mut())?;
+    let C = Buffer::<cl_float>::create(&context, CL_MEM_READ_WRITE, array_size, ptr::null_mut())?;
+    
+    let mut dur = 0;
+
+    for _ in 0..tests {
+        let (m1, m2) = init_mat(size);
+
+        let m = size;
+        let n = size;
+        let k = size;
+        let s = size as u32;
+
+        let _a_write_event = queue.enqueue_write_buffer(&mut A, CL_BLOCKING, 0, &m1.get_all(), &[])?;
+        let _x_write_event = queue.enqueue_write_buffer(&mut B, CL_BLOCKING, 0, &m2.get_all(), &[])?;
+
+        let kernel_event = yaif::kernel::get_mmul_kernel_event(kernel, queue, m as u32, n as u32, k as u32, &A, &B, &C)?;
+        
+        let mut events: Vec<cl_event> = Vec::default();
+        events.push(kernel_event.get());
+
+        let mut r1 = vec![0.0; array_size];
+        
+        let read_event = queue.enqueue_read_buffer(&C, CL_BLOCKING, 0, &mut r1, &events)?;
+        read_event.wait()?;
+        let mut r = Matrix::new(size, size);
+        r.fill_vec(&r1);
+
+        assert_eq!(r, m1.multiply(&m2).unwrap(), "OpenCL Matrix Matrix Multiplication is not working properly!");
+        
         let start_time = kernel_event.profiling_command_start()?;
         let end_time = kernel_event.profiling_command_end()?;
         let duration = end_time - start_time;
@@ -87,8 +195,21 @@ fn time_init(tests: usize, size: usize) {
     }
 }
 
+fn time_cl(kernel: &Kernel, context: &Context, queue: &CommandQueue, tests: usize, size: usize, which: &str) -> u64 {
+    let f = match which {
+        "add"=> test_add,
+        "smul" => test_smul,
+        "mmul" => test_mmul,
+        _ => unimplemented!("{}", which)
+    };
+    match f(&kernel, &context, &queue, tests, size) {
+        Ok(d) => { d },
+        Err(e) => panic!("{}", e),
+    }
+}
+
 fn main() -> Result<()> {
-    const TESTS: usize = 100;
+    const TESTS: usize = 1;
     let device_id = *get_all_devices(CL_DEVICE_TYPE_GPU)?
     .first()
     .expect("no device found in platform");
@@ -96,29 +217,49 @@ fn main() -> Result<()> {
     let context = Context::from_device(&device).expect("Context::from_device failed");
     let queue = CommandQueue::create_with_properties(&context, device_id, CL_QUEUE_PROFILING_ENABLE, 0)
         .expect("CommandQueue::create_default failed");
-    let program = Program::create_and_build_from_source(&context, PROGRAM_SOURCE, "")
+
+    let add_program = Program::create_and_build_from_source(&context, yaif::kernel::ADD_SOURCE, "")
         .expect("Program::create_and_build_from_source failed");
-    let kernel = Kernel::create(&program, KERNEL_NAME).expect("Kernel::create failed");
+    let add_kernel = Kernel::create(&add_program, yaif::kernel::ADD_NAME).expect("Kernel::create failed");
 
-    let mut actual_size = 1.0;
+    let mul_scalar_program = Program::create_and_build_from_source(&context, yaif::kernel::MUL_SCALAR_SOURCE, "")
+        .expect("Program::create_and_build_from_source failed");
+    let mul_scalar_kernel = Kernel::create(&mul_scalar_program, yaif::kernel::MUL_SCALAR_NAME).expect("Kernel::create failed");
 
-    while actual_size < (1 << 12) as f64 {
+    let mul_matrix_program = Program::create_and_build_from_source(&context, yaif::kernel::MUL_MATRIX_SOURCE, "")
+        .expect("Program::create_and_build_from_source failed");
+    let mul_matrix_kernel = Kernel::create(&mul_matrix_program, yaif::kernel::MUL_MATRIX_NAME).expect("Kernel::create failed");
+
+    let mut actual_size = 16.0;
+
+    while actual_size < (1 << 15) as f64 {
         let size = actual_size as usize;
-        let now = Instant::now();
-        time_init(TESTS, size);
-        let elapsed = now.elapsed();
-        println!("Init Matrix ({:4}x{:4}) {:?}", size, size, elapsed);
-        let cl_dur = match test_opencl(&kernel, &context, &queue, TESTS, size) {
-            Ok(d) => { d },
-            Err(e) => panic!("{}", e),
-        };
-        println!(" -- OpenCL matrix add ({:4}x{:4}): {:16?}ns", size, size, cl_dur);
-        let now = Instant::now();
-        test_solo(TESTS, size);
-        let solo_dur = (now.elapsed() - elapsed).as_nanos();
-        println!(" -- Basic matrix add ({:4}x{:4}):  {:16?}ns", size, size, solo_dur);
-        println!("Speed Up: {:.2}", solo_dur as f64 / cl_dur as f64);
-        actual_size *= 1.5;
+        // let now = Instant::now();
+        // time_init(TESTS, size);
+        // let elapsed = now.elapsed();
+        // println!("Init Matrix ({:4}x{:4}) {:?}", size, size, elapsed);
+        // let cl_dur = time_cl(&add_kernel, &context, &queue, TESTS, size, "add");
+        // println!(" -- OpenCL matrix matrix addition       ({:4}x{:4}): {:16?}ns", size, size, cl_dur);
+        // let now = Instant::now();
+        // time_basic(TESTS, size, "add");
+        // let basic_dur = now.elapsed().as_nanos() - elapsed.as_nanos();
+        // println!(" -- Basic matrix matrix addition        ({:4}x{:4}): {:16?}ns", size, size, basic_dur);
+
+        // let cl_dur = time_cl(&mul_scalar_kernel, &context, &queue, TESTS, size, "smul");
+        // println!(" -- OpenCL matrix scalar multiplication ({:4}x{:4}): {:16?}ns", size, size, cl_dur);
+        // let now = Instant::now();
+        // time_basic(TESTS, size, "smul");
+        // let basic_dur = now.elapsed().as_nanos() - elapsed.as_nanos();
+        // println!(" -- Basic matrix scalar multiplication  ({:4}x{:4}): {:16?}ns", size, size, basic_dur);
+        
+        let cl_dur = time_cl(&mul_matrix_kernel, &context, &queue, TESTS, size, "mmul");
+        println!(" -- OpenCL matrix matrix multiplication ({:5}x{:5}): {:16?}ns", size, size, cl_dur);
+        // let now = Instant::now();
+        // time_basic(TESTS, size, "mmul");
+        // let basic_dur = now.elapsed().as_nanos() - elapsed.as_nanos();
+        // println!(" -- Basic matrix matrix multiplication  ({:4}x{:4}): {:16?}ns", size, size, basic_dur);
+
+        actual_size *= 2.0;
     }
     Ok(())
 }
