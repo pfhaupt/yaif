eea85f850d13aeca9d413b16fb6aa305192692e1
pfhaupt <pfhaupt@gmx.net>
2023-07-17 22:10:38 +0200

    Prepare XOR test

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
+use yaif::data_set::{self, DataSet};
 use yaif::net::cl_net::ClNet;
+use yaif::net::NetTrait;
 use yaif::cl_kernel::ClStruct;
 
 fn main() {
+    let inputs = vec![vec![0.0, 0.0], vec![1.0, 0.0], vec![0.0, 1.0], vec![1.0, 1.0]];
+    let outputs = vec![vec![1.0, 0.0], vec![0.0, 1.0], vec![0.0, 1.0], vec![1.0, 0.0]];
+    let data_set = DataSet::new(&inputs, &outputs);
     let cl_struct = ClStruct::new().unwrap();
-    match ClNet::new(cl_struct, vec![5, 5]) {
-        Ok(cl) => {
+    match ClNet::new(cl_struct, vec![2, 1, 2]) {
+        Ok(mut cl) => {
+            cl.initialize_training_data(&data_set);
+            cl.run(true);
             println!("Worked!!")
         },
         Err(e) => {
