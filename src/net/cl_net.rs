528dcb53d124e76c59303b46708b950f0e6c572f
pfhaupt <pfhaupt@gmx.net>
2023-07-17 22:08:34 +0200

    Implement basic learning algorithm using GPU matrix operations
    It doesn't crash, but so far I don't know if it works properly.
    From debugging I've seen that it does *something*, which is better than
    nothing.
    Next step: Making sure it does exactly what it is supposed to do.

--- src/net/cl_net.rs
+++ src/net/cl_net.rs
@@ -10,12 +10,23 @@ pub struct ClNet {
     layer_count: usize,
     last_layer: usize,
 
+    // Buffers are 1D, but matrix ops need 2D, so I need to store each dimension
+    layer_dims: Vec<(usize, usize)>,
+    weight_dims: Vec<(usize, usize)>,
+    bias_dims: Vec<(usize, usize)>,
+    error_dims: Vec<(usize, usize)>,
+    active_dims: Vec<(usize, usize)>,
+
     // The magic, but this time with GPU buffers
     layers: Vec<Option<Buffer<f32>>>,
+    transposed_layers: Vec<Option<Buffer<f32>>>,
     weights: Vec<Option<Buffer<f32>>>,
+    transposed_weights: Vec<Option<Buffer<f32>>>,
     bias: Vec<Option<Buffer<f32>>>,
+    transposed_bias: Vec<Option<Buffer<f32>>>,
     errors: Vec<Option<Buffer<f32>>>,
     pre_activation: Vec<Option<Buffer<f32>>>,
+    der_activation: Vec<Option<Buffer<f32>>>,
 
     avg_weight: Vec<Option<Buffer<f32>>>,
     avg_bias: Vec<Option<Buffer<f32>>>,
@@ -41,12 +52,22 @@ impl Default for ClNet {
             layer_lengths: vec![],
             layer_count: 0,
             last_layer: 0,
+            
+            layer_dims: vec![],
+            weight_dims: vec![],
+            bias_dims: vec![],
+            error_dims: vec![],
+            active_dims: vec![],
 
             layers: vec![],
+            transposed_layers: vec![],
             weights: vec![],
+            transposed_weights: vec![],
             bias: vec![],
+            transposed_bias: vec![],
             errors: vec![],
             pre_activation: vec![],
+            der_activation: vec![],
 
             avg_weight: vec![],
             avg_bias: vec![],
@@ -71,33 +92,31 @@ impl NetTrait for ClNet {
         self.layer_lengths = layer_sizes.clone();
         self.layer_count = self.layer_lengths.len();
         self.last_layer = self.layer_count - 1;
+        
+        self.layer_dims = vec![(0, 0); self.layer_count];
+        self.weight_dims = vec![(0, 0); self.layer_count];
+        self.bias_dims = vec![(0, 0); self.layer_count];
+        self.error_dims = vec![(0, 0); self.layer_count];
+        self.active_dims = vec![(0, 0); self.layer_count];
 
-        self.layers = vec![];
-        for _ in 0..self.layer_count { self.layers.push(None); }
-       
-        self.weights = vec![];
-        for _ in 0..self.layer_count { self.weights.push(None); }
-        
-        self.bias = vec![];
-        for _ in 0..self.layer_count { self.bias.push(None); }
+        let fill_none = |v: &mut Vec<Option<Buffer<f32>>>| {
+            *v = vec![];
+            for _ in 0..self.layer_count { v.push(None); }
+        };
 
-        self.errors = vec![];
-        for _ in 0..self.layer_count { self.errors.push(None); }
-        
-        self.pre_activation = vec![];
-        for _ in 0..self.layer_count { self.pre_activation.push(None); }
-        
-        self.avg_weight = vec![];
-        for _ in 0..self.layer_count { self.avg_weight.push(None); }
-        
-        self.avg_bias = vec![];
-        for _ in 0..self.layer_count { self.avg_bias.push(None); }
-
-        self.bias_gradient = vec![];
-        for _ in 0..self.layer_count { self.bias_gradient.push(None); }
-        
-        self.weight_gradient = vec![];
-        for _ in 0..self.layer_count { self.weight_gradient.push(None); }
+        fill_none(&mut self.layers);
+        fill_none(&mut self.transposed_layers);
+        fill_none(&mut self.weights);
+        fill_none(&mut self.transposed_weights);
+        fill_none(&mut self.bias);
+        fill_none(&mut self.transposed_bias);
+        fill_none(&mut self.errors);
+        fill_none(&mut self.pre_activation);
+        fill_none(&mut self.der_activation);
+        fill_none(&mut self.avg_weight);
+        fill_none(&mut self.avg_bias);
+        fill_none(&mut self.bias_gradient);
+        fill_none(&mut self.weight_gradient);
 
         self.target_vector = self.cl_struct.create_buffer(self.layer_lengths[self.last_layer], 1);
         self.current_cost = self.cl_struct.create_buffer(self.layer_lengths[self.last_layer], 1);
@@ -119,29 +138,94 @@ impl NetTrait for ClNet {
         todo!()
     }
 
-    fn run(&mut self, debug: bool) -> bool {
+    fn learn(&mut self) {
+        self.calculate_layers();
+
+        self.calculate_cost();
+
+        self.calculate_derivations();
+
+        let (m, n) = self.active_dims[self.last_layer];
+        self.cl_struct.matrix_hadamard(&mut self.current_cost, &mut self.der_activation[self.last_layer], &mut self.errors[self.last_layer], m, n).unwrap();
+
+        // Backpropagation is now ready to go
+
+        for i in (1..(self.layer_count - 1)).rev() {
+            let (m, n) = self.weight_dims[i + 1];
+            self.cl_struct.matrix_transpose(&mut self.weights[i + 1], &mut self.transposed_weights[i + 1], m, n).unwrap();
+            
+            let (n1, m) = self.weight_dims[i + 1]; // reversed order because transposed
+            let (n2, k) = self.error_dims[i + 1];
+            assert_eq!(n1, n2);
+            let n = n1;
+
+            // TODO: This is just wrong on so many levels
+            // But I can't borrow self.errors[i] and self.errors[i + 1] at the same time...
+            // self.cl_struct.matrix_mult(&mut self.transposed_weights[i + 1], &mut self.errors[i + 1], &mut elf.errors[i], m, n, k).unwrap();
+            let mut bfr = self.cl_struct.create_buffer(n, k);
+            self.cl_struct.matrix_mult(&mut self.transposed_weights[i + 1], &mut self.errors[i + 1], &mut bfr, m, n, k).unwrap();
+            
+            let (m, n) = self.error_dims[i];
+            // Nevermind, the next line would've been bad anyway, no need to read the buffer and copy it over
+            // Another TODO: Outsource create_buffer, there's no reason to do it every single time
+            self.cl_struct.matrix_hadamard(&mut bfr, &mut self.der_activation[i], &mut self.errors[i], m, n).unwrap();
+        }
+
+        for i in 1..self.layer_count {
+            let (m, n) = self.layer_dims[i - 1];
+            self.cl_struct.matrix_transpose(&mut self.layers[i - 1], &mut self.transposed_layers[i - 1], m, n).unwrap();
+
+            let (m, n) = self.weight_dims[i];
+            self.cl_struct.matrix_dyadic(&mut self.errors[i], &mut self.transposed_layers[i - 1], &mut self.weight_gradient[i], m, n).unwrap();
+        }
+
+        for i in 0..self.layer_count {
+            let (m, n) = self.error_dims[i];
+            self.cl_struct.copy_buffer(&mut self.errors[i], &mut self.bias_gradient[i], m, n).unwrap();
+        }
+
+        for i in 0..self.layer_count {
+            let (m, n) = self.bias_dims[i];
+            self.cl_struct.matrix_add_inline(&mut self.avg_bias[i], &mut self.bias_gradient[i], m, n).unwrap();
+            let (m, n) = self.weight_dims[i];
+            self.cl_struct.matrix_add_inline(&mut self.avg_weight[i], &mut self.weight_gradient[i], m, n).unwrap();
+
+        }
+    }
+
+    fn guess(&mut self, input: &Vec<f32>) -> usize {
         todo!()
     }
 
-    fn learn(&mut self) {
-        todo!()
+    fn train(&mut self) {
+        let index = self.training_data.get_random_index();
+        let input = self.training_data.get_input(index).as_vec();
+        let output = self.training_data.get_output(index);
+        // println!("{:?} {:?}", input, output);
+        self.internal_train(&input, &output.as_vec());
     }
-    fn guess(&mut self, input: &Vec<f32>) -> usize {
+
+    fn adapt_weights(&mut self) {
         todo!()
     }
     
     fn set_target(&mut self, target: &Vec<f32>) {
-        todo!()
+        self.fill_buffer_vec(&self.layers[self.last_layer], target.len(), target.clone()).unwrap();
     }
+
     fn set_input(&mut self, input: &Vec<f32>) {
-        todo!()
+        self.fill_buffer_vec(&self.layers[0], input.len(), input.clone()).unwrap();
     }
 
     fn calculate_cost(&mut self) {
-        todo!()
+        let (m, n) = self.layer_dims[self.last_layer];
+        self.cl_struct.matrix_sub(&mut self.layers[self.last_layer], &mut self.target_vector, &mut self.current_cost, m, n).unwrap();
     }
+
     fn calculate_layers(&mut self) {
-        todo!()
+        for i in 1..self.layer_count {
+            self.calculate_layer(i);
+        }
     }
 
     fn print_guess(&mut self) {
@@ -171,8 +255,15 @@ impl ClNet {
             let len = self.layer_lengths[i];
 
             self.layers[i] = self.cl_struct.create_buffer(len, 1);
+            self.transposed_layers[i] = self.cl_struct.create_buffer(1, len);
+            self.layer_dims[i] = (len, 1);
+
             self.errors[i] = self.cl_struct.create_buffer(len, 1);
+            self.error_dims[i] = (len, 1);
+
             self.pre_activation[i] = self.cl_struct.create_buffer(len, 1);
+            self.der_activation[i] = self.cl_struct.create_buffer(len, 1);
+            self.active_dims[i] = (len, 1);
 
             self.bias_gradient[i] = self.cl_struct.create_buffer(len, 1);
         }
@@ -182,28 +273,23 @@ impl ClNet {
             let curr_len = self.layer_lengths[i];
 
             self.weights[i] = self.cl_struct.create_buffer(curr_len, prev_len);
+            self.transposed_weights[i] = self.cl_struct.create_buffer(prev_len, curr_len);
+            self.weight_dims[i] = (curr_len, prev_len);
 
             let two_over_input_count = 2.0 / (prev_len as f32);
             self.fill_buffer_gauss(&self.weights[i], curr_len * prev_len, 0.0, two_over_input_count)?;
 
             self.bias[i] = self.cl_struct.create_buffer(curr_len, 1);
-            self.fill_buffer(&self.bias[i], curr_len, 0.1)?;
+            self.transposed_bias[i] = self.cl_struct.create_buffer(1, curr_len);
+            self.bias_dims[i] = (curr_len, 1);
+
+            self.fill_buffer_scalar(&self.bias[i], curr_len, 0.1)?;
         }
 
-        for i in 1..self.layer_count {
-            let prev_len = self.layer_lengths[i - 1];
-            let curr_len = self.layer_lengths[i];
-            let r1 = self.cl_struct.read_buffer(&self.weights[i], prev_len * curr_len)?;
-            let r2 = self.cl_struct.read_buffer(&self.bias[i], curr_len)?;
-            println!("{:?}\n{:?}", r1, r2);
-        }
-
-        for i in 1..self.layer_count {
-            let prev_len = self.layer_lengths[i - 1];
-            let curr_len = self.layer_lengths[i];
-            let (b_row, b_col) = (curr_len, 1);
-            let (w_row, w_col) = (curr_len, prev_len);
-
+        for i in 0..self.layer_count {
+            let (b_row, b_col) = self.bias_dims[i];
+            let (w_row, w_col) = self.weight_dims[i];
+            
             self.avg_bias[i] = self.cl_struct.create_buffer(b_row, b_col);
             self.avg_weight[i] = self.cl_struct.create_buffer(w_row, w_col);
             self.weight_gradient[i] = self.cl_struct.create_buffer(w_row, w_col);
@@ -211,6 +297,37 @@ impl ClNet {
         Ok(())
     }
 
+    fn internal_train(&mut self, input: &Vec<f32>, output: &Vec<f32>) {
+        self.set_target(output);
+        self.set_input(input);
+        self.learn();
+    }
+
+    fn calculate_layer(&mut self, layer: usize) {
+        let (m, n1) = self.weight_dims[layer];
+        let (n2, k) = self.layer_dims[layer - 1];
+        assert_eq!(n1, n2);
+        let n = n1;
+        self.cl_struct.matrix_mult(&mut self.weights[layer], &mut self.layers[layer - 1], &mut self.pre_activation[layer], m, n, k).unwrap();
+        
+        let (m, n) = self.bias_dims[layer];
+        self.cl_struct.matrix_add_inline(&mut self.pre_activation[layer], &mut self.bias[layer], m, n).unwrap();
+        self.calculate_activation(layer);
+    }
+
+    fn calculate_derivations(&mut self) {
+        for i in 0..self.layer_count {
+            let (m, n) = self.active_dims[i];
+            self.cl_struct.der_sigmoid(&mut self.pre_activation[i], &mut self.der_activation[i], m, n).unwrap();
+        }
+    }
+
+    fn calculate_activation(&mut self, layer: usize) {
+        let (r, c) = self.active_dims[layer];
+        self.cl_struct.sigmoid(&mut self.pre_activation[layer], &mut self.layers[layer], r, c).unwrap()
+    }
+    
+    
     fn load_kernels(&mut self) {
         self.cl_struct.load_kernels();
     }
@@ -219,8 +336,12 @@ impl ClNet {
         self.cl_struct = cl_struct
     }
 
-    fn fill_buffer(&self, buffer: &Option<Buffer<f32>>, size: usize, val: f32) -> Result<(), ClError> {
-        self.cl_struct.fill(buffer, size, val)
+    fn fill_buffer_vec(&self, buffer: &Option<Buffer<f32>>, size: usize, values: Vec<f32>) -> Result<(), ClError> {
+        self.cl_struct.fill_vec(buffer, size, values)
+    }
+
+    fn fill_buffer_scalar(&self, buffer: &Option<Buffer<f32>>, size: usize, val: f32) -> Result<(), ClError> {
+        self.cl_struct.fill_scalar(buffer, size, val)
     }
 
     fn fill_buffer_gauss(&self, buffer: &Option<Buffer<f32>>, size: usize, mean: f32, variance: f32) -> Result<(), ClError> {
